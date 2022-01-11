2e83a2680b0f454f46a936a5ec353f64a12b471d
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-11 18:33:40 +0000

    initialized firebaset

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,56 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../firebase";
 
 export default function Home() {
+  const votesQuery = collection(db, "votes");
+  //Destructure user, loading and error out of the hook
+  const [user, loading, error] = useAuthState(auth);
+  //Destructure a listening snapshot, loading anf error out of the hook
+  const [snapshot, votesLoading, votesError] = useCollection(votesQuery, {});
+
+  //console.log the current user and the loading status
+  console.log("Loading :", loading, "|", "Current User :", user);
+
+  if (!votesLoading && snapshot) {
+    snapshot.docs.map((doc) => console.log(doc.data()));
+  }
+
+  //Create a document funtion
+  const addVoteDocument = async (vote) => {
+    const response = await setDoc(doc(db, "votes", user.uid), { vote: vote });
+  };
+
   return (
-    <div className={styles.container}>
-      <h1>Hello</h1>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button
+        onClick={() => {
+          addVoteDocument("yes");
+        }}
+        style={{ fontSize: 32, marginRight: 8 }}
+      >
+        ✅🍍🍕
+      </button>
+      <button
+        onClick={() => {
+          addVoteDocument("no");
+        }}
+        style={{ fontSize: 32 }}
+      >
+        ❌🍍🍕
+      </button>
     </div>
   );
 }
---
+++ pages/login.js
@@ -0,0 +1,45 @@
+/** @format */
+
+import React from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { auth } from "../firebase";
+import { GoogleAuthProvider } from "firebase/auth";
+import { useRouter, Router } from "next/router";
+
+const Login = () => {
+  const router = useRouter();
+
+  // Configure FirebaseUI.
+  const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: "popup",
+
+    // We will display Google as auth providers.
+    signInOptions: [GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: (user) => {
+        console.log({ user });
+        router.push("/");
+      },
+    },
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: "320px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Pineapple Login</h1>
+      <p>please sign in</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+    </div>
+  );
+};
+
+export default Login;
